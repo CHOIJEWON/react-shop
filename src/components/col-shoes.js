f47483f0a5cf7567961916c8566e22022bf6a3bf
CHOIJEWON <105709970+CHOIJEWON@users.noreply.github.com>
2024-01-05 16:23:08 +0900

    [feat] main page

---
+++ src/components/col-shoes.js
@@ -0,0 +1,15 @@
+import { Col } from "react-bootstrap"
+
+
+export function ColShoes(props) {
+    return props.shoes.map((shoe) => {
+        return (
+            <Col>
+                <img src={`${process.env.PUBLIC_URL}${shoe.img}`} width="80%" />
+                <h4> {shoe.title} </h4>
+                <p> {shoe.content} </p>
+                <p> {shoe.price} </p>
+            </Col>
+        )
+    })
+}
